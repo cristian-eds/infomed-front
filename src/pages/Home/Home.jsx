3392153d3f8458ac3f0a7a5fe4e39dad26f51f70
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-03-04 15:12:51 -0300

    Header page Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 
+import styles from './Home.module.css'
+
+import { FaSearch } from "react-icons/fa";
+
 const Home = ({logout}) => {
+
+  const date = new Date();
+
+
   return (
     <div>
       <Navbar logout={logout}/>
+      <main className={styles.container_main}>
+        <header className={styles.header}>
+          <div>
+            <h2>Hoje: {date && date.toLocaleDateString()}</h2>
+            <p>Próximo medicamento em 30 muinutos...</p>
+          </div>
+          <div className={styles.search_input}>
+            <input type="text" placeholder='Pesquisar...'/>
+            <FaSearch className={styles.search_input__icon}/>
+          </div>
+        </header>
+      </main>
     </div>
   )
 }
