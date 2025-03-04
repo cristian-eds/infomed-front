eea96f30753c3a0c1734702ac67bce64eceeaa68
Cristian Eduardo <cristian.santos0308@gmail.com>
2025-03-04 15:41:04 -0300

    Table of medicine in Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,14 @@ import styles from './Home.module.css'
 
 import { FaSearch } from "react-icons/fa";
 
-const Home = ({logout}) => {
+const Home = ({ logout }) => {
 
   const date = new Date();
 
 
   return (
     <div>
-      <Navbar logout={logout}/>
+      <Navbar logout={logout} />
       <main className={styles.container_main}>
         <header className={styles.header}>
           <div>
@@ -20,10 +20,42 @@ const Home = ({logout}) => {
             <p>Próximo medicamento em 30 muinutos...</p>
           </div>
           <div className={styles.search_input}>
-            <input type="text" placeholder='Pesquisar...'/>
-            <FaSearch className={styles.search_input__icon}/>
+            <input type="text" placeholder='Pesquisar...' />
+            <FaSearch className={styles.search_input__icon} />
           </div>
         </header>
+        <div className={styles.container_table}>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Número</th>
+                <th>Frequência</th>
+                <th>Horário</th>
+                <th>Concluído</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Ibuprofeno</td>
+                <td>1/10</td>
+                <td>3-3 horas</td>
+                <td>16:30</td>
+                <td></td>
+                <td>Editar</td>
+              </tr>
+              <tr>
+                <td>Ibuprofeno</td>
+                <td>1/10</td>
+                <td>3-3 horas</td>
+                <td>16:30</td>
+                <td></td>
+                <td>Editar</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </div>
   )
