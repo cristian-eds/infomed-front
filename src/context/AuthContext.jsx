7296e9ae94622de95838ac7e0ce71d4aa959ceeb
Cristian <cristian.santos0308@gmail.com>
2025-03-19 09:20:13 -0300

    Added react toastify to validations login

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,14 +4,37 @@ export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
 
+    const url = 'http://localhost:8080/auth/login';
+
     const [user, setUser] = useState(null);
 
-    const login = (userData) => {
-        setUser(userData);
+    const login = async (userData) => {   
+        const config = {
+            method: 'POST',
+            body: JSON.stringify(userData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        try {
+            const res = await fetch(url,config)
+                                .then(res => res.json());
+            console.log(res);
+            if(res.token) {
+                setUser(userData.login);
+                localStorage.setItem("token",res.token);
+                setUser(userData);
+            } else {
+                return res;
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     const logout = () => {
         setUser(null);
+        localStorage.removeItem("token");
     }
 
     return (
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
 
 import logo from '../../assets/react.svg'
 
@@ -8,7 +9,7 @@ import styles from './Login.module.css'
 import ButtonGroup from '../../components/Button/ButtonGroup';
 import Button from '../../components/Button/Button';
 
-const Login = ({login}) => {
+const Login = ({ login }) => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -16,34 +17,53 @@ const Login = ({login}) => {
 
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     setError(null);
+
     const user = {
       email,
       password
     }
-    login(user);
+
+    const res = await login(user);
+
+    if (res.status) {
+      toast(res.description);
+    }
   }
 
   return (
     <div className={styles.container_login}>
-        <div className="img">
-            <img src={logo} alt="Image Logo" />
-        </div>
-        <h2>Login</h2>
-        <form className={styles.login} onSubmit={handleLogin}>
-            <input type="text" name="email" id="email" placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" name="password" id="" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
-            {error && <p className={styles.error}>{error}</p>  }
-            
-            <ButtonGroup>
-              <Button type="submit" value="Entrar" variant="button_confirm"/>
-              <Button type="button" value="Registrar" variant="button_cancel" onClick={() => navigate("/register")}/>
-            </ButtonGroup>
-            
-            
-        </form>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="dark"
+        transition={Bounce}
+      />
+      <div className="img">
+        <img src={logo} alt="Image Logo" />
+      </div>
+      <h2>Login</h2>
+      <form className={styles.login} onSubmit={handleLogin}>
+        <input type="text" name="email" id="email" placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name="password" id="" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        {error && <p className={styles.error}>{error}</p>}
+
+        <ButtonGroup>
+          <Button type="submit" value="Entrar" variant="button_confirm" />
+          <Button type="button" value="Registrar" variant="button_cancel" onClick={() => navigate("/register")} />
+        </ButtonGroup>
+
+
+      </form>
     </div>
   )
 }
