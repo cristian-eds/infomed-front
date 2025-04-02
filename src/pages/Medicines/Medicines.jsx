1f9efcb8e0d4816c81a1f47cbc230fbad968f60d
Cristian <cristian.santos0308@gmail.com>
2025-04-02 11:21:51 -0300

    Fetching medicines on the page Medicine

--- src/pages/Medicines/Medicines.jsx
+++ src/pages/Medicines/Medicines.jsx
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 import InputSearch from '../../components/InputSearch/InputSearch';
+import Table from '../../components/Table/Table';
 
 import { MdDelete, MdEdit } from "react-icons/md";
 
+
+import { fetchMedicinesUser } from '../../slices/medicineSlice';
+
 import styles from './Medicines.module.css';
-import Table from '../../components/Table/Table';
+import { formatDate } from '../../utils/formatterDates';
 
 const Medicines = () => {
+
+    const dispatch = useDispatch();
+
+    const { loading, medicines } = useSelector(state => state.medicine);
+
+    console.log(medicines);
+
+    useEffect(() => {
+        const pagination = {
+            actualPage : '0',
+            sizePage : '6'
+          }
+        dispatch(fetchMedicinesUser(pagination))
+    }, [dispatch])
+
+    const generateRowsTableMedicine = (medicinesState) => {
+        console.log(medicinesState);
+        return medicinesState.map(medicine => (
+            <tr key={medicine.id}> 
+                <td>{medicine.name}</td>
+                <td>{formatDate(medicine.registrationDate)}</td>
+                <td>{medicine.frequencyHours}/{medicine.frequencyHours} horas</td>
+                <td>{medicine.totalDays} dias</td>
+                <td><input type="checkbox" name="conclusion" id="conclusion" /></td>
+                <td>
+                    <MdEdit />
+                    <MdDelete />
+                </td>
+            </tr>
+        ))
+    }
+
+    if(loading) return <p>Loading...</p>;
+
     return (
-        <main className="container_main">
+      
+      <main className="container_main">
             <header className={styles.header_medicines}>
                 <InputSearch />
             </header>
             <div>
                 <h3>Histórico medicamentos...</h3>
-                <Table titles={["Nome", "Data criação", "Frequência","Duração", "Concluído", "Ações"]}>
-                    <tr>
-                        <td>Ibuprofeno</td>
-                        <td>01/02/2025 18:00</td>
-                        <td>3-3horas</td>
-                        <td>5 dias</td>
-                        <td><input type="checkbox" name="conclusion" id="conclusion"/></td>
-                        <td>
-                            <MdEdit />
-                            <MdDelete />
-                        </td>
-                    </tr>
+                <Table titles={["Nome", "Data criação", "Frequência", "Duração", "Concluído", "Ações"]}>
+                    {medicines && generateRowsTableMedicine(medicines)}
                 </Table>
             </div>
         </main>
