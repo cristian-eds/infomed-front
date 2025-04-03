16c37e387fd87f8257825638ad0a6e52ce865152
Cristian <cristian.santos0308@gmail.com>
2025-04-03 11:14:20 -0300

    Created ArrowDownButton

---
+++ src/components/Button/ArrowDownButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { IoIosArrowDown } from 'react-icons/io';
+
+import styles from './Button.module.css';
+
+const ArrowDownButton = ({actionClick}) => {
+  return (
+    <button onClick={actionClick} className={`${styles.button} ${styles.button_back}`}>
+        <IoIosArrowDown />
+    </button>
+  )
+}
+
+export default ArrowDownButton
