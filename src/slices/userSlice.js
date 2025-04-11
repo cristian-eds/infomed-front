31a35d9b92daad5b28d3c9039c2b5dd8a0ea767f
Cristian <cristian.santos0308@gmail.com>
2025-04-11 15:37:10 -0300

    Created slice to update user

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
     user: {},
-    loading : false,
+    loading: false,
     error: null,
     success: null
 }
@@ -21,39 +21,58 @@ export const fetchUser = createAsyncThunk(
 
         const res = await fetch(`http://localhost:8080/users?email=${email}`, config)
             .then(res => res.json());
-        
+
         return res;
     }
 )
 
 export const changeUserPassword = createAsyncThunk(
     'user/changePassword',
-    async (data,thunkApi) => {
+    async (data, thunkApi) => {
         const token = localStorage.getItem("token");
-        const passwords = {
-            currentPassword: data.currentPassword,
-            newPassword: data.newPassword
-        }
         const config = {
             method: 'PATCH',
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Bearer ${token}`
             },
-            body: JSON.stringify(passwords)
+            body: JSON.stringify(data.atualization)
         }
 
         const res = await fetch(`http://localhost:8080/users/${data.id}`, config)
             .then(res => res);
-        
-        if(res.status == 400) {
+
+        if (res.status == 400) {
             let body = await res.json();
             return thunkApi.rejectWithValue(body.description);
-        } 
-        
-        if(res.status == 200) data.handleSuccessChangePassword();
+        }
+
+        if (res.status == 200) data.handleSuccessChangePassword();
 
         return res.status;
+    }
+)
+
+export const updateUser = createAsyncThunk(
+    'user/updateUser',
+    async (data) => {
+        const token = localStorage.getItem("token");
+        console.log(data);
+        const config = {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(data.user)
+        }
+
+        const res = await fetch(`http://localhost:8080/users/${data.id}`, config)
+            .then(res => res);
+
+        if (res.status == 200) data.handleSuccesUpdate();
+
+        return res.json();
     }
 )
 
@@ -70,40 +89,52 @@ export const userSlice = createSlice({
                 state.loading = true;
             }
         )
-        .addCase(
-            fetchUser.fulfilled,
-            (state, action) => {
-                state.user = action.payload;
-                state.loading = false;
-            }
-        )
-        .addCase(
-            fetchUser.rejected,
-            (state) => {
-                state.loading = false;
-                state.error = true;
-            }
-        )
-        .addCase(
-            changeUserPassword.pending,
-            (state) => {
-                state.loading = true;
-                state.error = null;
-            }
-        )
-        .addCase(
-            changeUserPassword.fulfilled,
-            (state) => {
-                state.loading = false;
-            }   
-        )
-        .addCase(
-            changeUserPassword.rejected,
-            (state, action) => {
-                state.loading = false;
-                state.error = action.payload;
-            }
-        )
+            .addCase(
+                fetchUser.fulfilled,
+                (state, action) => {
+                    state.user = action.payload;
+                    state.loading = false;
+                }
+            )
+            .addCase(
+                fetchUser.rejected,
+                (state) => {
+                    state.loading = false;
+                    state.error = true;
+                }
+            )
+            .addCase(
+                changeUserPassword.pending,
+                (state) => {
+                    state.loading = true;
+                    state.error = null;
+                }
+            )
+            .addCase(
+                changeUserPassword.fulfilled,
+                (state) => {
+                    state.loading = false;
+                }
+            )
+            .addCase(
+                changeUserPassword.rejected,
+                (state, action) => {
+                    state.loading = false;
+                    state.error = action.payload;
+                }
+            )
+            .addCase(
+                updateUser.pending,
+                (state) => {
+                    state.loading = true;
+                }
+            )
+            .addCase(
+                updateUser.fulfilled,
+                (state, action) => {
+                    state.user = action.payload;
+                }
+            )
     }
 })
 
