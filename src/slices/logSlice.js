61dc945f91b219413310605dd5bafbff2c1947b3
Cristian <cristian.santos0308@gmail.com>
2025-04-14 15:16:55 -0300

    Created slice to fetch more logs

--- src/slices/logSlice.js
+++ src/slices/logSlice.js
@@ -4,12 +4,24 @@ import { requestConfig } from "../utils/requests";
 const initialState = {
     logs: [],
     loading: false,
+    page: {},
     error: null,
     success: null
 }
 
 export const fetchLogs = createAsyncThunk(
     'log/fetchLogs',
+    async (pagination) => {
+        const config = requestConfig("GET");
+
+        const res = await fetch(`http://localhost:8080/log?actualPage=${pagination.actualPage}&sizePage=${pagination.sizePage}`, config)
+            .then(res => res.json());
+        return res;
+    }
+)
+
+export const fetchMoreLogs = createAsyncThunk(
+    'log/fetchMoreLogs',
     async (pagination) => {
         const config = requestConfig("GET");
 
@@ -38,9 +50,23 @@ export const logSlice = createSlice({
                 (state, action) => {
                     state.logs = action.payload.content;
                     state.loading = false;
+                    state.page = action.payload.page;
                 }
             )
-
+            .addCase(
+                fetchMoreLogs.pending,
+                (state) => {
+                    state.loading = true;
+                }
+            )
+            .addCase(
+                fetchMoreLogs.fulfilled,
+                (state, action) => {
+                    state.loading = false;
+                    state.logs.push(...action.payload.content);
+                    state.page = action.payload.page;
+                }
+            )
     }
 })
 
