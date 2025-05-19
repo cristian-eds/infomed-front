de23cf08108033aa84abc32a52b1ac192c314e90
Cristian <cristian.santos0308@gmail.com>
2025-05-19 10:48:22 -0300

    Added select to person on ModalAddMedicine

--- src/components/Modal/ModalAddMedicine.jsx
+++ src/components/Modal/ModalAddMedicine.jsx
@@ -9,14 +9,24 @@ import Button from '../Button/Button'
 
 import { format } from 'date-fns'
 import { convertToPatternLocalDateTime } from '../../utils/formatterDates'
+import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchPerson } from '../../slices/personSlice'
 
 
-const ModalAddMedicine = ({ showModal, setShowModal, actionToDispatch,dispatch }) => {
+const ModalAddMedicine = ({ showModal, setShowModal, actionToDispatch, dispatch }) => {
 
     const [name, setName] = useState("");
     const [frequency, setFrequency] = useState("");
     const [totalDays, setTotalDays] = useState("");
-    const [initialDate, setInitialDate] = useState(format(new Date(),'yyyy-MM-dd hh:mm'));
+    const [initialDate, setInitialDate] = useState(format(new Date(), 'yyyy-MM-dd hh:mm'));
+    const [idPerson, setIdPerson] = useState("");
+
+    const { personList } = useSelector(state => state.person);
+
+    useEffect(() => {
+        dispatch(fetchPerson())
+    }, [dispatch])
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -24,12 +34,17 @@ const ModalAddMedicine = ({ showModal, setShowModal, actionToDispatch,dispatch }
             name,
             totalDays,
             frequencyHours: frequency,
-            initialDateTime: convertToPatternLocalDateTime(initialDate)
+            initialDateTime: convertToPatternLocalDateTime(initialDate),
+            idPerson
         }
+
+        console.log(newMedicine);
 
         dispatch(actionToDispatch(newMedicine));
         setShowModal(false);
     }
+
+
 
     return (
         <>
@@ -41,18 +56,18 @@ const ModalAddMedicine = ({ showModal, setShowModal, actionToDispatch,dispatch }
                             <h2 className={styles.modal_content_header_text}>Nova Medicação</h2>
                         </header>
 
-                        <form className={styles.medication_form} onSubmit={handleSubmit}> 
+                        <form className={styles.medication_form} onSubmit={handleSubmit}>
                             <div className={styles.form_row}>
                                 <label htmlFor="name">Nome:</label>
                                 <div className={styles.input_group}>
-                                    <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)}/>
+                                    <input type="text" id="name" name="name" placeholder="Nome medicamento" value={name} onChange={(e) => setName(e.target.value)} />
                                 </div>
                             </div>
 
                             <div className={styles.form_row}>
                                 <label htmlFor="frequence">Frequência:</label>
                                 <div className={styles.input_group}>
-                                    <input type="number" id="frequence" name="frequence" value={frequency} onChange={(e) => setFrequency(e.target.value)} />
+                                    <input type="number" id="frequence" name="frequence" placeholder="Frequência" value={frequency} onChange={(e) => setFrequency(e.target.value)} />
                                     <span className="unit">hrs</span>
                                 </div>
                             </div>
@@ -60,7 +75,7 @@ const ModalAddMedicine = ({ showModal, setShowModal, actionToDispatch,dispatch }
                             <div className={styles.form_row}>
                                 <label htmlFor="qtdDias">Qtd Dias:</label>
                                 <div className={styles.input_group}>
-                                    <input type="number" id="qtdDias" name="qtdDias" value={totalDays} onChange={(e) => setTotalDays(e.target.value)} />
+                                    <input type="number" id="qtdDias" name="qtdDias" placeholder="Quantidade dias" value={totalDays} onChange={(e) => setTotalDays(e.target.value)} />
                                     <span className="unit">dias</span>
                                 </div>
                             </div>
@@ -68,13 +83,25 @@ const ModalAddMedicine = ({ showModal, setShowModal, actionToDispatch,dispatch }
                             <div className={styles.form_row}>
                                 <label htmlFor="dataHoraInicial">Dia e hora inicial:</label>
                                 <div className={styles.input_group}>
-                                    <input type="datetime-local" id="dataHoraInicial" name="dataHoraInicial" value={initialDate} onChange={(e) => setInitialDate(e.target.value) }  />
+                                    <input type="datetime-local" id="dataHoraInicial" name="dataHoraInicial" value={initialDate} onChange={(e) => setInitialDate(e.target.value)} />
+                                </div>
+                            </div>
+
+                            <div className={styles.form_row}>
+                                <label htmlFor="person">Pessoa vínculada:</label>
+                                <div className={styles.input_group}>
+                                    <select name="person" id="person" value={idPerson} onChange={(e) => setIdPerson(e.target.value)}>
+                                        <option value="">Selecione</option>
+                                        {personList.map(person => (
+                                            <option key={person.id} value={person.id}>{person.name}</option>
+                                        ))}
+                                    </select>
                                 </div>
                             </div>
 
                             <ButtonGroup>
                                 <Button value="Cadastrar" type="submit" variant="button_confirm" />
-                                <Button value="Cancelar" type="button" onClick={()=> setShowModal(false)} variant="button_cancel"/>
+                                <Button value="Cancelar" type="button" onClick={() => setShowModal(false)} variant="button_cancel" />
                             </ButtonGroup>
                         </form>
                     </div>
