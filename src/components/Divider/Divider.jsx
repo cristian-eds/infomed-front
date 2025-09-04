c3327f60a4c4fe5536759a4b528e162e9e8b79d7
Cristian <cristian.santos0308@gmail.com>
2025-09-04 15:31:07 -0300

    Created Divider component

---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Divider.module.css'
+
+const Divider = () => {
+  return (
+    <div className={styles.container_divider}></div>
+  )
+}
+
+export default Divider
